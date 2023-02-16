4e30784e2a6c584cd2e165340e2e874c086f3a1e
Xingwang Liao <kuoruan@gmail.com>
2023-02-16 13:13:38 +0800

    fix: fix proxy environment variables

--- src/server/proxy-agent.ts
+++ src/server/proxy-agent.ts
@@ -5,7 +5,9 @@ import logger from "./logger";
 declare const GLOBAL_AGENT: Record<"HTTP_PROXY" | "HTTPS_PROXY" | "NO_PROXY", string>;
 
 export function registerGlobalProxyAgent() {
-  bootstrap();
+  bootstrap({
+    environmentVariableNamespace: "",
+  });
 
   const config = JSON.stringify(GLOBAL_AGENT || {});
   logger.info({ config }, "using proxy config: @{config}");
