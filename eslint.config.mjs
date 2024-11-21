d2f6e26611f2f77b8a861aa7f44d85b6b130db0e
Xingwang Liao <kuoruan@gmail.com>
2024-11-21 22:30:24 +0800

    chore: use next resolver for eslint import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import eslint from "@eslint/js";
+import importResolverNext from "eslint-import-resolver-next";
 import pluginImportX from "eslint-plugin-import-x";
 import pluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import pluginSimpleImportSort from "eslint-plugin-simple-import-sort";
@@ -69,12 +70,13 @@ export default tseslint.config(
       "@typescript-eslint/no-base-to-string": "off",
     },
     settings: {
-      "import-x/resolver": {
-        node: {},
-        typescript: {
-          project: "./tsconfig.json",
+      "import-x/resolver": [
+        {
+          name: "next",
+          resolver: importResolverNext,
+          options: {},
         },
-      },
+      ],
     },
   },
   {
