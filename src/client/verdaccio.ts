c874ce61ae686dee9c5d89d05eb6945c7b95ddd3
Xingwang Liao <kuoruan@gmail.com>
2025-01-19 22:15:55 +0800

    fix: update login dialog selector for verdaccio 5

--- src/client/verdaccio.ts
+++ src/client/verdaccio.ts
@@ -1,7 +1,7 @@
 import { init } from "./plugin";
 
 const loginButtonSelector = `[data-testid="header--button-login"]`;
-const loginDialogSelector = `[data-testid="dialogContentLogin"]`;
+const loginDialogSelector = `[data-testid="login--dialog"] .MuiDialogContent-root,[data-testid="dialogContentLogin"]`;
 const logoutButtonSelector = `[data-testid="header--button-logout"],[data-testid="logOutDialogIcon"]`;
 const usageTabsSelector = `[data-testid="tab-content"]`;
 
