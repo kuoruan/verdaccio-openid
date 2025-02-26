5ca05476ae5b924a1b34db684babc48e0200cd0e
Xingwang Liao <kuoruan@gmail.com>
2025-02-26 14:41:41 +0800

    chore: update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import eslint from "@eslint/js";
 import { createNextImportResolver } from "eslint-import-resolver-next";
 import pluginImportX from "eslint-plugin-import-x";
+import pluginPerfectionist from "eslint-plugin-perfectionist";
 import pluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import pluginSimpleImportSort from "eslint-plugin-simple-import-sort";
 import pluginUnicorn from "eslint-plugin-unicorn";
 import globals from "globals";
 import tseslint, { configs } from "typescript-eslint";
@@ -10,11 +10,12 @@ import tseslint, { configs } from "typescript-eslint";
 export default tseslint.config(
   eslint.configs.recommended,
   pluginPrettierRecommended,
-  pluginUnicorn.configs["flat/recommended"],
+  pluginUnicorn.configs["recommended"],
   pluginImportX.flatConfigs.recommended,
   pluginImportX.flatConfigs.typescript,
   ...configs.recommendedTypeChecked,
   ...configs.stylisticTypeChecked,
+  pluginPerfectionist.configs["recommended-natural"],
   {
     ignores: ["node_modules/", "dist/", ".history/"],
   },
@@ -27,39 +28,26 @@ export default tseslint.config(
       },
       parserOptions: {
         ecmaVersion: "latest",
+        projectService: true,
         sourceType: "module",
         warnOnUnsupportedTypeScriptVersion: false,
-        projectService: true,
       },
     },
-    plugins: {
-      "simple-import-sort": pluginSimpleImportSort,
-    },
     rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-
-      "unicorn/no-null": "off",
-      "unicorn/no-process-exit": "off",
-      "unicorn/catch-error-name": "off",
-      "unicorn/filename-case": "off",
-      "unicorn/prefer-module": "off",
-      "unicorn/prevent-abbreviations": "off",
-      "unicorn/prefer-global-this": "off",
-
-      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-base-to-string": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-import-type-side-effects": "error",
       "@typescript-eslint/no-misused-promises": [
         "error",
         {
           checksVoidReturn: false,
         },
       ],
-      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+
+      "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
@@ -67,7 +55,14 @@ export default tseslint.config(
           varsIgnorePattern: "^_",
         },
       ],
-      "@typescript-eslint/no-base-to-string": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "unicorn/catch-error-name": "off",
+      "unicorn/filename-case": "off",
+      "unicorn/no-null": "off",
+      "unicorn/no-process-exit": "off",
+      "unicorn/prefer-global-this": "off",
+      "unicorn/prefer-module": "off",
+      "unicorn/prevent-abbreviations": "off",
     },
     settings: {
       "import-x/resolver": [],
